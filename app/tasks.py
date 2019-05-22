1bc8c21ef30af3c767a74131c5e6d95c19251893
Bjoern Stiel <bjoern.stiel@zoomeranalytics.com>
2019-05-22 18:28:20 +0200

    Update tasks.py

--- app/tasks.py
+++ app/tasks.py
@@ -2,6 +2,9 @@ from datetime import datetime
 from worker import app
 
 
+print(f'Loading {__name__}')
+
+
 @app.task(bind=True, name='get_time')
 def get_time(self):
     return datetime.now().isoformat()
